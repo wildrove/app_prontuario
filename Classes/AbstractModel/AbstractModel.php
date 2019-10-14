85702157fd93e2da87244dca892a0bd7ce6c8339
wilder <wilderagner@gmail.com>
2019-10-14 15:45:29 -0300

    Conexão com Firebird funcionando com PDO

---
+++ Classes/AbstractModel/AbstractModel.php
@@ -0,0 +1,6 @@
+<?php
+namespace Classes\AbstractModel;
+
+	abstract class AbstractModel{
+
+	}
--- Classes/FirebirdConnection/FirebirdConnection.php
+++ Classes/FirebirdConnection/FirebirdConnection.php
@@ -1,8 +1,29 @@
 <?php
 namespace Classes\FirebirdConnection;
+use PDO;
 
 	class FirebirdConnection {
+		public $conn = null;
 
+		public function __construct()
+		{
+			$this->createConnection('firebird', 'localhost:/BancoFirebird/SGH.FDB', 'SYSDBA', 'masterkey');
+		}
+
+		private function createConnection($driverName, $hostName, $user, $pass)
+		{
+			try{
+
+				$this->conn = new \PDO("$driverName:dbname=$hostName", $user, $pass);
+				$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				echo '<h1>Conectado com sucesso!</h1>';
+
+			}catch(PDOException $e){
+				echo 'Não foi possível conectar!' . $e->getMessage();
+			}
+		}
+
+	/*
 		$hostName = "localhost:/BancoFirebird/SGH.FDB";
 		$user = "SYSDBA";
 		$pass = "masterkey";
@@ -16,6 +37,8 @@ namespace Classes\FirebirdConnection;
 		}catch(Execption $e){
 			echo "Não foi possível conectar" . $e->getMessage();
 		}
+	*/
+		
 	}
 
 
