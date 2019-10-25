134bf101917de7a37ab74a4c50a626ef2fdcb27d
wilder <wilderagner@gmail.com>
2019-10-25 15:49:15 -0300

    Paginação Implementada 25/10 15:50

--- Actions/Pacient/findPacient.php
+++ Actions/Pacient/findPacient.php
@@ -13,7 +13,7 @@
    // pega a pagina atual
 	$pagina = (isset($_GET['pagina'])) ? (int)$_GET['pagina'] : 1;
 	//itens por página
-	$quantidade = 10;
+	$quantidade = 20;
    // calcula o inicio da consulta
 	$inicio = ($pagina * $quantidade) - $quantidade;
    // função de consulta no banco
@@ -26,29 +26,33 @@
 
 	$pagina_anterior = $pagina -1;
 	$pagina_posterior = $pagina + 1;
+   $registro = null;
+
 	?>
 	<!DOCTYPE html>
 	<html>
 	<head>
 	  <title>Listar Pacientes</title>
 	  <meta charset="utf-8">
+      <!-- Link Personal style.css -->
+     <link rel="stylesheet" type="text/css" href="../../bootstrap/css/style.css">
 	    <!-- Bootstrap -->  
 	  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	  <!-- Link Personal style.css -->
-	  <link rel="stylesheet" type="text/css" href="../../bootstrap/css/style.css">
 
 	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 	  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 	</head>
 		<body>
-			<div class="container-fluid">
+			<div class="container">
             <?php 
                require_once '../../forms/headerUserPacientList.php';
-            ?>   
-			    <table class="table shadow-lg" style="margin-top: 180px;">
+            ?>
+            <h1 class="text-center mb-3" style="margin-top: 130px">Lista de Pacientes</h1>    
+            <div>
+			    <table class="table shadow-lg table-hover table-striped table-bordered">
 			        <thead class="thead-dark">
-			          <tr class="text-center" style="font-size: 15px;">
+			          <tr class="text-center" style="font-size: 15px">
 			            <th scope="col" class="border-right">REG. PRONTUÁRIO</th>
 			            <th scope="col" class="border-right">NOME PACIENTE</th>
 			            <th scope="col" class="border-right">DT NASCIMENTO</th>
@@ -62,28 +66,29 @@
 			        <?php 
 			        foreach($result_pagina as $rowPacient) {
 			            ?>
-			            <tr class="text-center border">
-			              <th scope="row" class="border-right"><?php echo $rowPacient['REGISTRO_PRONTUARIO']; ?></th>
+			            <tr class="text-center border font-italic">
+			              <th scope="row" class="border-right "><?php echo $rowPacient['REGISTRO_PRONTUARIO']; ?></th>
 			              <td class="border-right"><?php echo $rowPacient['NOME']; ?></td>
 			              <td class="border-right"><?php echo date('d-m-Y', strtotime($rowPacient['DATA_NASCIMENTO'])); ?></td>
 			              <td class="border-right"><?php echo $rowPacient['DOCUMENTO']; ?></td>
 			              <td class="border-right"><?php echo $rowPacient['NOME_MAE']; ?></td>
 			              <td class="border-right"><?php echo $rowPacient['TELEFONE']; ?></td>
 			              <td>
-			                <a href="#" class="btn btn-primary">Pesquisar</a>
+			                <a href="findProntuary.php?registro=<?php echo $rowPacient['REGISTRO_PRONTUARIO'] ?>" class="btn btn-primary">Pesquisar</a>
 			              </td>
 			            </tr>
 			            <?php
 			        }?>
 			        </tbody>
-			    </table>
+			     </table>
+            </div>
 				<nav align="center" aria-label="Page navigation" style="margin-bottom: 20px;">
-					<ul class="pagination">
+					<ul class="pagination mt-3">
 					   <li class="page-item <?php if($pagina_anterior == 0){ echo 'disabled';} ?>">
 					   <?php 
 					   if($pagina_anterior != 0) { ?>
 					      <a href="findPacient.php?pagina=<?php echo $pagina_anterior; ?>&paciente=<?php if(isset($_GET['paciente']))($_SESSION['nomeP'] = $name); echo $_SESSION['nomeP']; ?>&data=<?php if(isset($_GET['dtNasc']))($_SESSION['data'] = $birthday); echo $_SESSION['data']; ?>" style="text-decoration: none;">
-					         <span class="page-link" aria-hidden="true">Anterior</span>
+					         <span class="page-link bg-primary text-light" aria-hidden="true">Anterior</span>
 					      </a>
 					   <?php } else { ?>
 					         <span class="page-link" >Anterior</span>
@@ -95,30 +100,34 @@
 					            echo "<li class='page-item'><span class='page-link' aria-hidden='true'>...</li>";
 					        }
 					        if($pagina > 1){
-					        echo "<li class='page-item'><a class='page-link' href='findPacient.php?pagina=".$pagina_anterior."'>".$pagina_anterior."</a></li>";
+					        echo "<li class='page-item'><a class='page-link' href='findPacient.php?pagina=".$pagina_anterior."&paciente=".$_SESSION['nomeP']."&data=".$_SESSION['data']."'>".$pagina_anterior."</a></li>";
 					        }
-					        echo "<li class='page-item active'><a class='page-link' href='findPacient.php?pagina=".$pagina."'>".$pagina."</a></li>";
+					        echo "<li class='page-item active'><a class='page-link' href=''>".$pagina."</a></li>";
 					        if($pagina_posterior <= $num_pagina){
-					          echo "<li class='page-item'><a class='page-link' href='findPacient.php?pagina=".$pagina_posterior ."'>".$pagina_posterior."</a></li>"; 
+					          echo "<li class='page-item'><a class='page-link' href='findPacient.php?pagina=".$pagina_posterior ."&paciente=".$_SESSION['nomeP']."&data=".$_SESSION['data']."'>".$pagina_posterior."</a></li>"; 
 					        }
 					        if($pagina_posterior < $num_pagina){
 					            echo "<li class='page-item'><span class='page-link' aria-hidden='true'>...</li>";
 					        }
 					        ?>
-					   <li class="page-item <?php if($pagina_posterior > $num_pagina){echo 'disabled';} ?>">
+					      <li class="page-item <?php if($pagina_posterior > $num_pagina){echo 'disabled';} ?>">
 					       <?php 
 					       if($pagina_posterior <= $num_pagina) { ?>
 					          <a href="findPacient.php?pagina=<?php echo $pagina_posterior; ?>&paciente=<?php if(isset($_GET['paciente']))($_SESSION['nomeP'] = $name); echo $_SESSION['nomeP']; ?>&data=<?php if(isset($_GET['dtNasc']))($_SESSION['data'] = $birthday); echo $_SESSION['data']; ?>" aria-label="Previous" style="text-decoration: none">
-					             <span class="page-link" aria-hidden="true">Próximo</span>
+					             <span class="page-link bg-primary text-light" aria-hidden="true">Próximo</span>
 					          </a>
 					       <?php } else { ?>
 					          <span class="page-link" aria-hidden="true">Próximo</span>
 					        <?php } ?>
-					    </li>
+					     </li>
 					</ul>
 				</nav>
 				<a class="btn btn-primary  mb-5 shadow-lg" href="../../home.php">Voltar</a>
 			</div>
 	</body>
-</html>	
+</html>
+
+<?php
+
+echo $registro;	
 
---
+++ Actions/Pacient/findProntuary.php
@@ -0,0 +1,4 @@
+<?php
+
+$pacientProntuary = intval($_GET['registro']);
+var_dump($pacientProntuary);
