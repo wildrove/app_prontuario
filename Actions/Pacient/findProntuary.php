1cf3563b8c61c41c00e86a8b22faa7018e77aa48
Wilder <wilderagner@gmail.com>
2020-03-17 15:58:48 -0300

    Ultima atualização importante

--- Actions/Pacient/findProntuary.php
+++ Actions/Pacient/findProntuary.php
@@ -46,10 +46,10 @@
 	<meta charset="ISO-8859-1">
     <!-- Bootstrap Online -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!-- Link Personal style.css -->
-    <link rel="stylesheet" type="text/css"  href="../../bootstrap/css/style.css">
 	<!-- Bootstrap Local -->  
-	<link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css"> 
+	<link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
+	<!-- Link Personal style.css -->
+    <link rel="stylesheet" type="text/css"  href="../../bootstrap/css/style.css">
     
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
