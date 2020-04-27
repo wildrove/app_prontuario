963673fe854a23af13e5fd1a589322491c37eda7
Wilder <wilderagner@gmail.com>
2020-04-27 16:53:59 -0300

    Update 17:00 27/04/2020

--- Actions/Pacient/findProntuary.php
+++ Actions/Pacient/findProntuary.php
@@ -6,15 +6,20 @@
 
 	use Classes\Pacient\PacientEvolution\PacientEvolution;
 
-	$pacientRegistry = intval($_GET['regProntuary']);
+	$pacientProntuary = intval($_GET['regProntuary']);
+	$pacientRegistry = (isset($_GET['regPacient']) ? intval($_GET['regPacient']) : "");
 	$hourEvo = $_GET['hourEvolution'];
 	$dateEvo = $_GET['dateEvolution'];
+	$pacientName = (isset($_GET['pacientName']) ? $_GET['pacientName'] : "");
+	$mother = (isset($_GET['mother']) ? $_GET['mother'] : "");
+	$birthday = (isset($_GET['birthday']) ? date('d/m/Y', strtotime($_GET['birthday'])) : "");
+	$type = (isset($_GET['type']) ? $_GET['type'] : "");
 
 
 	$pacientEvolution = new PacientEvolution();
 
 	// Procura a evolução do paciente na tabela PEP_EVOLUCAO_MEDICA OU EVOLUCAO_WARELINE (SISTEMA ANTIGO)
-	$pacientEvo = $pacientEvolution->pacientEvo($pacientRegistry,$dateEvo,$hourEvo);
+	$pacientEvo = $pacientEvolution->pacientEvo($pacientProntuary,$dateEvo,$hourEvo);
 
 	
 	// VERIFICA SE A CONSULTA NÃO RETORNA VAZIO
@@ -43,89 +48,96 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
-	<!-- Bootstrap Local -->  
-	<link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
-	<!-- Link Personal style.css -->
-    <link rel="stylesheet" href="../../css/estilo.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<title></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet"  href="../../bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet"  href="../../css/estilo.css">
+	<script type="text/javascript">
+		function printPage(){
+			window.print();
+		}
+	</script>
 </head>
 <body>
-	<?php  
-		include '../../forms/headerPacient.php';
-	 ?>
-	<div class="container-fluid">		
-		<section class="" style="margin-top: 130px;"><!-- Sessão de cabeçalho -->
-				<form class="form text-form bg-light shadow-lg p-5" method="post" action="dataPrint.php">
-					<h4 class="">Dados do Paciente</h4>
-					<div class="row p-2 border border-dark mb-3" style="font-family: Arial, Helvetica, sans-serif;"><!-- Inicio Linha 1 -->
-						<div class="form-group pacient-group">
-							<label class="col-form-label">Paciente:</label>
-							<input class="form-control-plaintext input-pacient" type="text" name="nomePaciente">
-						</div>
-						<div class="form-group pacient-group">
-							<label class="col-form-label">Dt. Nasc:</label>
-							<input class="form-control-plaintext input-pacient" type="date" name="dtNascimento">
-						</div>
-						<div class="form-group pacient-group">
-							<label class="col-form-label">Mãe:</label>
-							<input class="form-control-plaintext input-pacient" type="text" name="nomeMae">
-						</div>
-						<div class="form-group pacient-group">
-							<label class="col-form-label">Prontuário:</label>
-							<input class="form-control-plaintext input-pacient" type="text" name="prontuario">
-						</div>
-						<div class="form-group pacient-group">
-							<label class="col-form-label">Reg. Paciente:</label>
-							<input class="form-control-plaintext input-pacient" type="text" name="regPaciente">
-						</div>
-						<div class="form-group pacient-group">
-							<label class="col-form-label">Dt. Evolução:</label>
-							<input class="form-control-plaintext input-pacient" type="date" name="dtEvo">
-						</div>
-						<div class="form-group pacient-group">
-							<label class="col-form-label">Tipo Evolução:</label>
-							<input class="form-control-plaintext input-pacient" type="text" name="tipoEvo">
-						</div>
-						<div class="form-group">
-							<textarea name="evolucao" style="display: none; border: none;">
-								<?php 
-									foreach ($pacientEvo as $value) {
-										
-										echo $value['EVOLUCAO'];
-									}
-								?>	
-							</textarea>
-						</div>
-					</div><!-- Fim Linha 1 -->
-					<div class="row border border-dark p-3"><!-- Inicio Linha 2 -->
-						<div class="form-group">
-							<h1 style="text-align: center;">Evolução do paciente</h1>
-							<p class="text-break">
-								<?php 
-									foreach ($pacientEvo as $value) {
-										echo "<pre style='font-family: Arial;font-size: 75%;'>";
-										echo wordwrap($value['EVOLUCAO'], 250, "<br><br>", true);
-									}
-								?>	
-							</p>
-						</div>
-					</div><!-- Fim Linha 2 -->
-					<div class="form-group">
-						<button class="btn btn-primary" type="submit">Enviar</button>
+	<div class="container-fluid font-pacient-type">
+		<section style="border: 1px solid #000000">
+			<div class="row">
+				<div class="col-sm">
+					<img class="p-2" src="../../img/hospital-header-logo.png">
+				</div>
+				<div class="col-sm p-3">
+					<div>
+						<h3 style="font-size: 25px;">HOSPITAL MATERNIDADE SÃO LUCAS</h3>
+						<h6>Rua: Mauri Bueno de Andrade Nº 101 - Extrema / MG</h6>
+						<h6 class="float-center">Fone: (35) 3100-9550</h6>			
 					</div>
-				</form>
-		</section><!-- Fim Sessão cabeçalho -->
-		<div class="p-5">
-			<button type="button" class="btn btn-primary btn-lg" onclick="goBack()">Voltar</button>
-			<a href="exportEvoDoc.php?regProntuary=<?php echo $pacientRegistry . '&' . 'hourEvolution=' . $hourEvo . '&' . 'dateEvolution=' . $dateEvo;  ?>" class="btn btn-primary btn-lg ml-5">Baixar Evolução</a>
-		</div>
+				</div>
+			</div>
+			<div class="row d-flex justify-content-center">
+				<h4 class="">Resumo de Evolução</h4>
+			</div>
+		</section><!-- Fim Sessão Hospital -->
+
+		<section class="mt-2" style="border: 1px solid #000000"><!-- Sessão Paciente -->
+			<div class="row m-2">
+				<h4>Dados Paciente</h4>
+			</div>
+			<div class="row p-4"><!-- Linha 1 -->
+				<div class="form-group pacient-group">
+					<label class="col-form-label">Paciente:</label>
+					<input class="form-control-plaintext input-pacient" type="text" name="nomePaciente" value="<?php echo $pacientName ?>"  disabled="" style="width: auto">
+				</div>
+				<div class="form-group pacient-group">
+					<label class="col-form-label">Dt. Nasc:</label>
+					<input class="form-control-plaintext input-pacient" type="text" name="dtNascimento" value="<?php echo $birthday ?>" disabled="">
+				</div>
+				<div class="form-group pacient-group">
+					<label class="col-form-label">Mãe:</label>
+					<input class="form-control-plaintext input-pacient" type="text" name="nomeMae" value="<?php echo $mother ?>" disabled="" style="width: 400px">
+				</div>
+				<div class="form-group pacient-group">
+					<label class="col-form-label">Prontuário:</label>
+					<input class="form-control-plaintext input-pacient" type="text" name="prontuario" value="<?php echo $pacientProntuary ?>" disabled="">
+				</div>
+				<div class="form-group pacient-group">
+					<label class="col-form-label">Reg. Paciente:</label>
+					<input class="form-control-plaintext input-pacient" type="text" name="regPaciente" value="<?php echo $pacientRegistry ?>" disabled="">
+				</div>
+				<div class="form-group pacient-group">
+					<label class="col-form-label">Dt. Evolução:</label>
+					<input class="form-control-plaintext input-pacient" type="text" name="dtEvo" value="<?php echo date('d/m/Y', strtotime($dateEvo)) ?>" disabled="">
+				</div>
+				<div class="form-group pacient-group">
+					<label class="col-form-label">Tipo Evolução:</label>
+					<input class="form-control-plaintext input-pacient" type="text" name="tipoEvo" value="<?php echo $type ?>" disabled="">
+				</div>
+				<div class="row pacient-discription border-top border-dark"><!-- Inicio Texto descrição -->
+					<p class="text-break">
+						<?php 
+							foreach ($pacientEvo as $value) {
+								echo "<pre style='font-family: Arial;font-size: 73%;'>";
+								echo wordwrap($value['EVOLUCAO'], 300, "<br><br>", true);
+							}	
+						?>	
+					</p>
+				</div><!-- Fim texto Descrição -->	
+			</div><!-- Fim Linha 1 -->
+		</section><!-- Fim Sessão Paciente -->
+		<button class="btn btn-primary btn-lg mt-5 mb-5" type="button" name=""onclick="goBack()">Voltar</button>
+		<button class="btn btn-primary btn-lg mt-5 mb-5" type="button" onclick="imprimir();">Imprimir</button>
+
+		<a href="exportEvoDoc.php?regProntuary=<?php echo $pacientProntuary . '&' . 'hourEvolution=' . $hourEvo . '&' . 'dateEvolution=' . $dateEvo;  ?>" class="btn btn-primary btn-lg">Baixar Evolução</a>
 	</div>
+	
 	<script type="text/javascript">
 		function goBack(){
 			window.history.go(-1);
+		}
+	</script>
+
+	<script>
+		function imprimir(){
+			window.print();
 		}
 	</script>
 </body>
